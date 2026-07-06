74f9eb92201752820ac86e7a88f1ae746a0f78d1
zyqzyq <zyqzyq08@qq.com>
2026-07-06 19:39:17 +0800

    fix tauir build error

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -8,6 +8,9 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    dedupe: ["react", "react-dom", "@tanstack/react-query"],
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
