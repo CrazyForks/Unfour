01fb909f8fcd6331a6786300601b0b67b6f61b22
zyqzyq <zyqzyq08@qq.com>
2026-07-06 11:21:36 +0800

    rm design sync

--- .design-sync/previews/StatusBadge.tsx
+++
@@ -1,6 +0,0 @@
-import { StatusBadge } from '@unfour/ui';
-
-export const Neutral = () => <StatusBadge>Unknown</StatusBadge>;
-export const Success = () => <StatusBadge tone="success">Active</StatusBadge>;
-export const Warning = () => <StatusBadge tone="warning">Pending</StatusBadge>;
-export const Danger = () => <StatusBadge tone="danger">Failed</StatusBadge>;
